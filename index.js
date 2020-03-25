d2e90dcdbd08a22311190d74b73e6ca9fb5a46ff
Caio Martins <caiomartins>
2020-03-25 15:16:48 -0300

    Métodos HTTP e Tipos de Parâmetros

--- index.js
+++ index.js
@@ -2,9 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) =>{
+
+app.use(express.json())
+
+/**
+ * Rota / Recurso
+ */
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar um informação do backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ * 
+ */
+
+ /**
+  * Tipos de Parâmetros
+  * 
+  * Query Parms: Parâmetros nomeados na rota apos "?" (Filtros, Paginação)
+  * Route Parms: Parâmetros utilizados para indentificar recursos
+  * Request Body: Corpo da requisição, utilizad para criar ou 
+  * 
+  */
+
+app.post('/users/', (request, response) =>{
+    const body = request.body;
+    console.log(body);
+
     return response.json({
-        evento: 'Semana OmmiStack 11.0'
+        evento: 'Semana OmmiStack 11.0',
+        aluno: 'Diego Fernandes'
     })
 })
 
