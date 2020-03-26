c8df2a4683e7f18183dd8172761d7bbdd1504acc
Caio Martins <caiomartins>
2020-03-26 09:54:51 -0300

    Criando SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const OngController = require('./constroller/OngController');
 const IncidentsController = require('./constroller/IncidentsController');
+const ProfileController = require('./constroller/ProfileController');
+const SessionController = require('./constroller/SessionController');
+
 const routes = express.Router();
+
+routes.post('/session', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index)
 
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
