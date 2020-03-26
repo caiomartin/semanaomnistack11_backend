104a3306eb15b620c693b579103f450e5f522c79
Caio Martins <caiomartins>
2020-03-26 09:36:04 -0300

    Criando IncidentsController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const OngController = require('./constroller/OngController');
+const IncidentsController = require('./constroller/IncidentsController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
 
 module.exports = routes;
